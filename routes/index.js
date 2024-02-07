f71b94543bb07ec62871d083890a19bd5262e6f7
AYZBTR <bhattaraiaayush.78@gmail.com>
2024-02-08 00:54:23 +0200

    How do I find documents where an array field contains all of a set of values?

--- routes/index.js
+++ routes/index.js
@@ -14,19 +14,27 @@ router.get('/create', async function(req, res){
   nickname: "DVD",
   description: "Hello My name is David . I am 31 years old.",
 
-  categories: ["Java", "spring", "html", "css", "express", "mongoDB"]
+  categories: ["Java", "spring", "html", "mongoose", "express", "mongoDB"]
  });
  res.send(userData1 );
 });
 
 
-//How can I perform a case-insensitive search in Mongoose?
-router.get('/find', async function(req, res){
+//Question1: How can I perform a case-insensitive search in Mongoose?
+
+/* router.get('/find', async function(req, res){
   //it will change the name to insensitive way so that if we search the username in any way either in capital or small we will get the user!
   // so ^aayush$ it means that search for this word exactly this word it doesnotmatter 
   //which way it's written ( i.e capital or small)
   var regex = new RegExp("^aayush$", 'i') ;   
   let findUser = await userModel.find({username: regex});
+  res.send(findUser);
+}); */
+
+
+//Question 2: How do I find documents where an array field contains all of a set of values?
+router.get('/find', async function(req, res){
+  let findUser = await userModel.find({categories:{$all:["mongoose"]}});
   res.send(findUser);
 });
 
